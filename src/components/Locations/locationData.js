af890442a06629cfe02595e556936a9690a7e8f5
wowimnick <65257805+wowimnick@users.noreply.github.com>
2025-02-04 13:32:42 -0500

    Update locationData.js

--- src/components/Locations/locationData.js
+++ src/components/Locations/locationData.js
@@ -5,7 +5,7 @@ export const locations = [
     address: '9428 Baymeadows Rd, Suite 230, Jacksonville, FL 32256',
     phone: '(904) 733-1717',
     fax: '(904) 733-1737',
-    email: 'jacksonvillereferrals@confidentcare.com',
+    email: 'intakejax@confidentcare.com',
     coordinates: { lat: 30.3322, lng: -81.6557 }
   },
   {
@@ -14,7 +14,7 @@ export const locations = [
     address: '3 Pine Cone Drive, # 107, Palm Coast, FL 32137',
     phone: '(386) 439-5440',
     fax: '(386) 439-5449',
-    email: 'flagler@confidentcare.com',
+    email: 'palmcoast@confidentcare.com',
     coordinates: { lat: 29.5542, lng: -81.2086 }
   },
   {
@@ -23,7 +23,7 @@ export const locations = [
     address: '6971 N. Federal Highway Suite 403, Boca Raton, FL 33487',
     phone: '(561) 886-6533',
     fax: '(561) 952-4164',
-    email: 'WPBreferrals@confidentcare.com',
+    email: 'sfoffice@confidentcare.com',
     coordinates: { lat: 26.4127, lng: -80.0707 }
   },
   {
@@ -32,7 +32,7 @@ export const locations = [
     address: '1920 E Hallandale Beach Blvd. Ste. 906, Hallandale Beach, FL 33009',
     phone: '(954) 454-8999',
     fax: '(954) 903-4162',
-    email: 'broward@confidentcare.com',
+    email: 'sfoffice@confidentcare.com',
     coordinates: { lat: 25.9872, lng: -80.1489 }
   },
   
@@ -42,7 +42,7 @@ export const locations = [
     address: '17100 Collins Ave., Suite 223, Sunny Isles Beach, FL 33160',
     phone: '(305) 933-9337',
     fax: '(954) 903-4162',
-    email: 'miami@confidentcare.com',
+    email: 'sfoffice@confidentcare.com',
     coordinates: { lat: 25.9366, lng: -80.1225 }
   },
   
